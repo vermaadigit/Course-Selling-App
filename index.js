8ddd7fc876fa4744a6cdfa67db3bed3d20834112
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-10 22:52:46 +0530

    Routes Added

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 const express = require('express')
+const { createUserRoutes } = require('./user')
+const { createCourseRoutes } = require('./Routes/course')
 const app = express()
 
-app.post('/user/signup', function(req, res) {
-    res.json({
-        message : 'signup endpoint'
-    })
-})
-
-app.post('/user/signin', function(req, res) {
-    res.json({
-        message : 'signin endpoint'
-    })
-})
-
-app.get('/user/purchases', function(req, res) {
-    res.json({
-        message : 'Purchases Endpoint'
-    })
-})
-
-app.post('/course/purchase', function(req, res) {
-    res.json({
-        message : 'Purchase Endpoint'
-    })
-})
-
-app.get('/courses', function(req, res) {
-    res.json({
-        message : 'Courses Endpoint'
-    })
-})
-
+createUserRoutes(app)
+createCourseRoutes(app)
 
 app.listen(3000)
