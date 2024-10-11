284eb31e86cba13d8f8900f3a415690fc941f4b9
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-11 16:52:19 +0530

    Connect When Server is UP

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const { userRouter } = require('./Routes/user')
 const { adminRouter } = require('./Routes/admin')
 const { courseRouter } = require('./Routes/course')
@@ -10,4 +11,10 @@ app.use("/api/v1/admin", adminRouter)
 app.use("/api/v1/course", courseRouter)
 
 
-app.listen(3000)
+async function main() {
+    await mongoose.connect("mongodb+srv://admin:Aditya112233@courseappserver.lq0xy.mongodb.net/coursera-app")
+    app.listen(3000)
+    console.log('Listing on port 3000')
+}
+
+main()
