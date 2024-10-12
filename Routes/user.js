1b09ab7b1ec31ef389f3b2c5aa31aef62c5070f7
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-12 15:24:01 +0530

    User Signin and Signup Endpoint Schema Added

--- Routes/user.js
+++ Routes/user.js
@@ -1,17 +1,51 @@
 const { Router } = require("express")
+const { userModel } = require("../db")
+const jwt = require("jsonwebtoken")
+const JWT_USER_PASSWORD = "vitserve"
 
 const userRouter = Router()
 
-    userRouter.post('/signup', function(req, res) {
+    userRouter.post('/signup', async function(req, res) {
+        const { email, password, firstName, lastName } = req.body // Todo : Adding Zod validation
+        // Todo : hash the password so plaintext pw is not stored in DB
+
+        // Todo :  Put it in Try catch Block
+        await userModel.create({
+            email : email,
+            password : password,
+            firstName : firstName,
+            lastName : lastName
+        })
+
+
         res.json({
-            message : 'signup Endpoint'
+            message : 'Signup Succeeded'
         })
     })
     
-    userRouter.post('/signin', function(req, res) {
-        res.json({
-            message : 'signin Endpoint'
+    userRouter.post('/signin', async function(req, res) {
+        const { email, password } = req.body
+
+        //Todo : ideally password should be hashed, and hence you can't compare the user provided password and the database password 
+        const user = await userModel.findOne({
+            email : email,
+            password : password
         })
+
+        if (user) {
+            const token = jwt.sign({
+                id : user._id
+            }, JWT_USER_PASSWORD)
+
+            res.json ({
+                token : token
+            })
+        } else {
+            res.status(403).json({
+                message : 'Incorrect Credentials'
+            })
+        }
+        
     })
     
     userRouter.get('/purchases', function(req, res) {
