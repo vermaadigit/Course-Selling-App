d45a96ac26312325239baf6e17f9948fbba4b5bc
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-12 22:13:02 +0530

    dotenv variable added in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const JWT_USER_PASSWORD = "vitserve"
-const JWT_ADMIN_PASSWORD = "notserve"
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
 
 module.exports = {
     JWT_USER_PASSWORD,
