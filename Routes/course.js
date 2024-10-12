c20edb8b0172d21bd36977abeb0b8af88e96928c
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-13 01:52:41 +0530

    Added Preview Endpoint

--- Routes/course.js
+++ Routes/course.js
@@ -1,15 +1,28 @@
 const { Router } = require("express")
+const { userMiddleware } = require("../middleware/user")
+const { purchaseModel, courseModel } = require("../db")
 const courseRouter = Router()
 
-    courseRouter.post('/purchase', function(req, res) {
+    courseRouter.post('/purchase', async function(req, res) {
+        const userId = req.userId
+        const courseId = req.body.courseId
+
+        const purchase = await purchaseModel.create({
+            courseId,
+            userId
+        })
+
         res.json({
-            message : 'Purchase Endpoint'
+            message : 'You have Successfully bought the course'
         })
     })
     
-    courseRouter.get('/preview', function(req, res) {
+    courseRouter.get('/preview', async function(req, res) {
+
+        const courses = await courseModel.find({})
+        
         res.json({
-            message : 'Courses Endpoint'
+            courses
         })
     })
 
