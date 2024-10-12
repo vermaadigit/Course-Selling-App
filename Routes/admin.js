da7ea22792285e9faee4150b4b5409bff9cb95bb
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-12 15:22:30 +0530

    Admin Signup and Signin Endpoint

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,32 +1,63 @@
 const { Router } = require("express")
 const adminRouter = Router()
 const { adminModel } = require("../db")
+const jwt = require("jsonwebtoken")
+const JWT_ADMIN_PASSWORD = "notserve"
 
-adminRouter.post('/signup', function(req, res) {
+adminRouter.post('/signup', async function(req, res) {
+    const { email, password, firstName, lastName } = req.body; // Todo : Adding Zod validation
+    // Todo : hash the password so plaintext pw is not stored in DB
+
+    // Todo :  Put it in Try catch Block
+    await adminModel.create({
+        email : email,
+        password : password,
+        firstName : firstName,
+        lastName : lastName
+    })
+
     res.json({
-        message : 'signup Endpoint'
+        message : 'Signup Succeeded'
     })
 })
 
-adminRouter.post('/signin', function(req, res) {
-    res.json({
-        message : 'signin Endpoint'
+adminRouter.post('/signin', async function(req, res) {
+    const { email, password } = req.body
+
+    //Todo : ideally password should be hashed, and hence you can't compare the user provided password and the database password 
+    const admin = await adminModel.findOne({
+        email : email,
+        password : password
     })
+
+    if (admin) {
+        const token = jwt.sign({
+            id : admin._id
+        }, JWT_ADMIN_PASSWORD)
+
+        res.json ({
+            token : token
+        })
+    } else {
+        res.status(403).json({
+            message : 'Incorrect Credentials'
+        })
+    }
 })
 
-adminRouter.post('/', function(req, res) {
+adminRouter.post('/course', function(req, res) {
     res.json({
         message : 'Purchases Endpoint'
     })
 })
 
-adminRouter.put('/', function(req, res) {
+adminRouter.put('/course', function(req, res) {
     res.json({
         message : 'Purchases Endpoint'
     })
 })
 
-adminRouter.get('/bulk', function(req, res) {
+adminRouter.get('/course/bulk', function(req, res) {
     res.json({
         message : 'Purchases Endpoint'
     })
