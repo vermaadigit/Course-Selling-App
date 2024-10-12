114ea0dc4d52d8b92d6a4422f86806ac7f6c2762
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-10-12 20:44:19 +0530

    User Routes Added

--- Routes/user.js
+++ Routes/user.js
@@ -1,7 +1,8 @@
 const { Router } = require("express")
 const { userModel } = require("../db")
 const jwt = require("jsonwebtoken")
-const JWT_USER_PASSWORD = "vitserve"
+const { JWT_USER_PASSWORD } = require("../config")
+
 
 const userRouter = Router()
 
